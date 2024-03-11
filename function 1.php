c7404c9336b5e83aba8c734115be280c2f7890c0
George <g.m.6@live.com>
2024-03-11 19:30:33 +0200

    Inital - function 2

---
+++ function 1.php
@@ -0,0 +1,6 @@
+<?php
+
+function evaluateOperation($operation)
+{
+
+}
---
+++ function2.php
@@ -0,0 +1,20 @@
+<?php
+
+$testString = 'hpd12aq3@8w$5 ';
+
+function filterNumbers ($string)
+{
+    $numbers = [];
+
+    for ($i=0; $i<strlen($string); $i++) {
+        if (ctype_digit($string[$i])) {
+            array_push($numbers,$string[$i]);
+        }
+    }
+
+    return $numbers;
+}
+
+print_r(filterNumbers($testString));
+
+?>
